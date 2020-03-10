f68871572572debb4673c59cd1d4ca49d6b17e94
joonseongpark <rytt@yonsei.ac.kr>
2020-03-11 02:05:54 +0900

    My Landing Page (Menu Slider & Modal)

---
+++ My Landing Page/script.js
@@ -0,0 +1,21 @@
+const toggle = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
+const openBtn = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+console.log(modal);
+// Toggle nav
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+
+// Show modal
+openBtn.addEventListener("click", () => modal.classList.add("show-modal"));
+
+// Hide modal
+closeBtn.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+// Hide modal on outside click
+window.addEventListener("click", e =>
+  e.target == modal ? modal.classList.remove("show-modal") : false
+);
